4f52f06135e8b6b3a5210c402fc2ecf09a9a8b29
UrtziFM <urtzi.arana13@gmail.com>
2021-03-06 19:03:05 +0100

    Iterating in the exercise

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
-#define N 5
+#define Size 40
+#define Note 5
+#define Row 2
 
 int main() {
-    float a[N] = {9.3, 9.0, 7.4, 8.0, 7.7};
+    int i,j;
+
+    float Matriz[Row][Note] ={{9.3, 9.0, 7.4, 8.0, 7.7}, {1, 2, 3, 4, 5}};
+    float Array[Note] = {9.3, 9.0, 7.4, 8.0, 7.7};
     float sum = 0.0;
     
-    for (int i = 0; i < N; i++) {
-        sum = sum + a[i];
+    for (i = 0; i < Note; i++) {
+        sum = sum + Array[i];
     }
-    printf("The average Note is = %f. \n", sum/5);
+    printf("The average note is = %f. \n", sum/5);
+
+    for (i = 0; i < Note; i++){
+        for (j = 0; j < Row; j++){
+            printf("%f. \t", Matriz[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
