48437b6560b00c295a03acdb143238e081850b7b
UrtziFM <urtzi.arana13@gmail.com>
2021-03-06 10:29:19 +0100

    exercise 1 finished

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+#define N 5
+
+int main() {
+    float a[N] = {9.3, 9.0, 7.4, 8.0, 7.7};
+    float sum = 0.0;
+    
+    for (int i = 0; i < N; i++) {
+        sum = sum + a[i];
+    }
+    printf("The average Note is = %f. \n", sum/5);
+    return 0;
+}
